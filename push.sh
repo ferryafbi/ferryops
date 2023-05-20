b1dae937ce6b44973285c81cd7bfb2928f348064
ferryafbi <ferry.a.febian@gmail.com>
2023-05-20 20:30:24 +0800

    new post (2023-05-20 20:30:24)

--- push.sh
+++ push.sh
@@ -1,1 +1,10 @@
-git add .;git commit -m "new post";git push
+#!/bin/bash
+
+tanggal=$(date +'%Y-%m-%d')
+waktu=$(date +'%H:%M:%S')
+
+pesan_commit="new post ($tanggal $waktu)"
+
+git add .
+git commit -m "$pesan_commit"
+git push
