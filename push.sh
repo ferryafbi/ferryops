e843d5ad7d9f7520bf33a3add13ae6eab83f180e
ferryafbi <ferry.a.febian@gmail.com>
2023-05-20 23:01:17 +0800

    new post (2023-05-20 23:01:17)

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ tanggal=$(date +'%Y-%m-%d')
 waktu=$(date +'%H:%M:%S')
 
 pesan_commit="new post ($tanggal $waktu)"
+# output new post (2023-05-20 20:30:24)
 
 git add .
 git commit -m "$pesan_commit"
