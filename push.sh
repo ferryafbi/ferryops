ea336b9a070a86f8749178c3f6f9b021c233e634
ferryafbi <ferry.a.febian@gmail.com>
2023-05-21 23:49:43 +0800

    new post (2023-05-21 23:49:43)

--- push.sh
+++ push.sh
@@ -6,7 +6,7 @@ waktu=$(date +'%H:%M:%S')
 pesan_commit="new post ($tanggal $waktu)"
 # output new post (2023-05-20 20:30:24)
 
-npm run build
+# npm run build
 git add .
 git commit -m "$pesan_commit"
 git push
