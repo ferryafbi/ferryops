dad34432f51f8438a02433eb9bb342510b0ea756
ferryafbi <ferry.a.febian@gmail.com>
2023-06-10 12:43:36 +0800

    add ua parser

--- pages/utils/userAgent.ts
+++ pages/utils/userAgent.ts
@@ -2,9 +2,7 @@ import { UAParser } from 'ua-parser-js'
 
 const parser = new UAParser()
 
-const parseUserAgent = (userAgent: string) => {
+export default function parseUserAgent(userAgent: string) {
   parser.setUA(userAgent)
   return parser.getResult()
 }
-
-export default parseUserAgent
