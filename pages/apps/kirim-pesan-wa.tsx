087c9b534623e842306acf8a6486a79c97dc5a68
Ferry Ananda Febian <ferry@debian.debian.org>
2023-06-24 20:36:47 +0800

    kirim pesan wa

--- pages/apps/kirim-pesan-wa.tsx
+++ pages/apps/kirim-pesan-wa.tsx
@@ -7,27 +7,25 @@ export default function KirimPesanWa() {
       alert('Masukkan nomor')
       return
     }
-    window.open(
-      `https://web.whatsapp.com/send/?phone=${value}&text&type=phone_number&app_absent=0`
-    )
+    window.open(`https://wa.me/${value}`)
   }
   return (
     <>
-      <div className="mb-2 flex justify-center ">
+      <div className="mt-4 mb-2 flex justify-center ">
         <h1>Kirim Pesan Wa</h1>
       </div>
       <div className="mb-2 flex justify-center">
         <input
-          type="text"
+          type="number"
           placeholder="62"
-          className="w-96 rounded-full border-2 border-solid border-sky-500 p-1"
+          className="w-96 rounded-full border-2 border-solid border-sky-500 p-3"
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
       </div>
       <div className="flex justify-center">
         <button
-          className="w-24 rounded-full bg-slate-400 decoration-white"
+          className="w-24 h-auto rounded-full bg-slate-400 text-white hover:bg-slate-600"
           onClick={sendWhatsapp}
         >
           Kirim
