c1c6fb551c0b50ae5a7c4df2f68fb0108df58696
Ferry Ananda Febian <ferry.a.febian@gmail.com>
2023-06-29 16:41:24 +0800

    new post and add copy button

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -8,15 +8,25 @@
  *
  */
 import { PortableText } from '@portabletext/react'
-
+import CopyToClipboard from 'react-copy-to-clipboard'
 import styles from './PostBody.module.css'
 import SyntaxHighlighter from 'react-syntax-highlighter'
+import { BiCopy } from 'react-icons/bi'
 
 export default function PostBody({ content }) {
   const serializers = {
     types: {
       code: ({ value }: any) => (
         <div className="my-2" key={value.key}>
+          <div className="flex h-8 items-center justify-between bg-neutral-950 px-1 text-white">
+            <span>{value.language || "text"}</span>
+            <CopyToClipboard
+              text={value.code}
+              onCopy={() => alert('udah disalin bang')}
+            >
+              <BiCopy />
+            </CopyToClipboard>
+          </div>
           <SyntaxHighlighter language={value.language}>
             {value.code}
           </SyntaxHighlighter>
